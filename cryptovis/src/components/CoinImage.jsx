0c995b0c4765f2a899278cb93e8e04f8bc8e6cc7
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-10-14 00:06:43 +0300

    styling

--- cryptovis/src/components/CoinImage.jsx
+++ cryptovis/src/components/CoinImage.jsx
@@ -3,16 +3,21 @@ import styled, {css} from 'styled-components'
 
 const CoinImage = styled.img`
 height: 50px;
+filter: white;
+border-radius: 50%;
+background-color: white;
 ${props => props.spotlight && css`
 height: 300px;
 margin: auto;
 display: block;
+border-radius: 50%;
+background-color: white;
 `}
 `
 
 
 export default function({coin, spotlight}) {
-    
+   
     return (
         <CoinImage
         spotlight={spotlight}
